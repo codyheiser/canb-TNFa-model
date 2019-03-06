8f24d1aca6b43301db21d8922e8f23ec46b23a6e
Cody Heiser <codyheiser49@gmail.com>
2019-03-06 14:39:08 -0600

    adding annotated diagram of model

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 ### canb-TNFa-model
-Modeling TNFa - NFkB signaling after [Lipniacki, et. al. (2007)](https://bmcbioinformatics.biomedcentral.com/articles/10.1186/1471-2105-8-376#Sec8) using [PySB](https://github.com/LoLab-VU/pysb)
+Modeling TNFa - NFkB signaling after [Lipniacki, et. al. (2007)](https://bmcbioinformatics.biomedcentral.com/articles/10.1186/1471-2105-8-376#Sec8) using [PySB](https://github.com/LoLab-VU/pysb)  
+
+---
+Ensure dependencies are installed in your environment:  
+```bash
+pip install -r requirements.txt
+```
+---
+##### Markup of model diagram with rate IDs
+![diag](diagram_markup.png)
--- tnfa_model.py
+++ tnfa_model.py
@@ -5,18 +5,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 Model() # creates the 'model' object
-
+# define monomers, binding sites, and states
 Monomer('TNFa', ['t'])
 Monomer('TNFR1', ['t', 'tn'])
 Monomer('IKKK', ['i', 'tn', 'state'], {'state':['n', 'a']})
 Monomer('IKK', ['i','a','ik','in','state'], {'state':['n','a','i','ii']})
 Monomer('A20', ['tn','i'])
-Monomer('IkBa', ['n','i','state'],{'state':['n','c']})
+Monomer('IkBa', ['n','i','loc','phos'], {'loc':['n','c'], 'phos':['p','u']})
 Monomer('NFkB', ['n','d','state'],{'state':['n','c']})
 Monomer('DNA', ['a','ikb'])
 Monomer('A20_mRNA', ['n'])
 Monomer('IkBa_mRNA', ['n'])
-
+# define initial parameter values for rates
 Parameter('TNFa_init', 10)
 Parameter('TNFR1_init', 100)
 Parameter('IKKK_init', 68)
@@ -27,6 +27,7 @@ Parameter('NFkB_init', 68)
 Parameter('DNA_init', 10)
 Parameter('A20_mRNA_init', 0)
 Parameter('IkBa_mRNA_init', 0)
+# define initial values
 Initial(TNFa(t=None),TNFa_init)
 Initial(TNFR1(t=None, tn=None),TNFR1_init)
 Initial(IKKK(i=None, tn=None, state='n'), IKKK_init)
@@ -37,17 +38,19 @@ Initial(NFkB(n=1, d=None, state='c'), NFkB_init)
 Initial(DNA(a=None, ikb=None), DNA_init)
 Initial(A20_mRNA(n=None))
 Initial(IkBa_mRNA(n=None))
-
-
-Parameter('kf', 1)
-Parameter('kr', 1000)
-Parameter('kif', 15)
-Parameter('kir', 100)
+# define parameter values for rates
+Parameter('kb', 1) # receptor activation rate
+Parameter('kf', 1) # receptor inactivation rate
+Parameter('ka', 1) # IKKK activation rate
+Parameter('ki', 1000) # IKKK inactivation rate
+Parameter('c1', 15) # inducible A20 and IkB mRNA synthesis
+Parameter('c3', 15) # A20 and IkB mRNA degradation
+# define rules to build ODES
 Rule('E_binds_S', E(b=None) + S(b=None) | E(b=1) % S(b=1), kf, kr)
 Parameter('kcat', 100)
 Rule('ES_to_P', E(b=1) % S(b=1) >> E(b=None) + P(), kcat)
 Rule('E_binds_I', E(b=None) + I(b=None) | E(b=1) % I(b=1), kif, kir)
-
+# define observable values for output
 Observable('E_free', E(b=None))
 Observable('S_free', S(b=None))
 Observable('ES_complex', E(b=1)% S(b=1)) # E(b=1) % S(b=1)
@@ -57,10 +60,12 @@ Observable('I_total', I())
 Observable('EI_complex', E(b=1) % S(b=1))
 Observable('I_free', I())
 
+# print back objects
 print(model.rules)
 print(model.parameters)
 print(model.observables)
 
+# solve equations over time tspan
 tspan = np.linspace(0, 10, 101)
 print(tspan)
 x = odesolve(model, tspan, verbose=True)
@@ -69,8 +74,8 @@ print(model.species)
 for rxn in model.reactions:
     print(rxn)
 
+# plot observable results
 plt.figure('My first PySB model')
-
 for obs in model.observables:
     plt.plot(tspan, x[obs.name], lw=2, label=obs.name)
 

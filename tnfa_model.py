d2d563960c8d6070af8824f8a1559d09c8cebf57
Cody Heiser <codyheiser49@gmail.com>
2019-03-11 14:41:02 -0500

    updates from 11Mar class

--- tnfa_model.py
+++ tnfa_model.py
@@ -4,80 +4,152 @@ from pysb.integrate import odesolve
 import numpy as np
 import matplotlib.pyplot as plt
 
-Model() # creates the 'model' object
-# define monomers, binding sites, and states
-Monomer('TNFa', ['t'])
-Monomer('TNFR1', ['t', 'tn'])
-Monomer('IKKK', ['i', 'tn', 'state'], {'state':['n', 'a']})
-Monomer('IKK', ['i','a','ik','in','state'], {'state':['n','a','i','ii']})
-Monomer('A20', ['tn','i'])
-Monomer('IkBa', ['n','i','loc','phos'], {'loc':['n','c'], 'phos':['p','u']})
-Monomer('NFkB', ['n','d','state'],{'state':['n','c']})
-Monomer('DNA', ['a','ikb'])
-Monomer('A20_mRNA', ['n'])
-Monomer('IkBa_mRNA', ['n'])
-# define initial parameter values for rates
-Parameter('TNFa_init', 10)
-Parameter('TNFR1_init', 100)
-Parameter('IKKK_init', 68)
-Parameter('IKK_init', 10)
-Parameter('A20_init', 100)
-Parameter('IkBa_init', 68)
-Parameter('NFkB_init', 68)
-Parameter('DNA_init', 10)
-Parameter('A20_mRNA_init', 0)
-Parameter('IkBa_mRNA_init', 0)
-# define initial values
-Initial(TNFa(t=None),TNFa_init)
-Initial(TNFR1(t=None, tn=None),TNFR1_init)
-Initial(IKKK(i=None, tn=None, state='n'), IKKK_init)
-Initial(IKK(i=None, a=None, ik=None, in=None, state='n'), IKK_init)
-Initial(A20(tn=None, i=None), A20_init)
-Initial(IkBa(n=1,i=None, state='c'),IkBa_init)
-Initial(NFkB(n=1, d=None, state='c'), NFkB_init)
-Initial(DNA(a=None, ikb=None), DNA_init)
-Initial(A20_mRNA(n=None))
-Initial(IkBa_mRNA(n=None))
-# define parameter values for rates
-Parameter('kb', 1) # receptor activation rate
-Parameter('kf', 1) # receptor inactivation rate
-Parameter('ka', 1) # IKKK activation rate
-Parameter('ki', 1000) # IKKK inactivation rate
-Parameter('c1', 15) # inducible A20 and IkB mRNA synthesis
-Parameter('c3', 15) # A20 and IkB mRNA degradation
-# define rules to build ODES
-Rule('E_binds_S', E(b=None) + S(b=None) | E(b=1) % S(b=1), kf, kr)
-Parameter('kcat', 100)
-Rule('ES_to_P', E(b=1) % S(b=1) >> E(b=None) + P(), kcat)
-Rule('E_binds_I', E(b=None) + I(b=None) | E(b=1) % I(b=1), kif, kir)
-# define observable values for output
-Observable('E_free', E(b=None))
-Observable('S_free', S(b=None))
-Observable('ES_complex', E(b=1)% S(b=1)) # E(b=1) % S(b=1)
-Observable('P_total', P())
-Observable('E_total', E()) # matches E(b=None) and E(b=1) % S(b=1)
-Observable('I_total', I())
-Observable('EI_complex', E(b=1) % S(b=1))
-Observable('I_free', I())
+Model()
+# creates the 'model' object
 
-# print back objects
-print(model.rules)
-print(model.parameters)
-print(model.observables)
+Monomer('TNFa', ['tnfr1'])
+Monomer('TNFR1', ['tnfa'])
+Monomer('IKKK', ['state'], {'state': ['n', 'a']})
+Monomer('IKK', ['state'], {'state': ['n', 'a', 'i', 'ii']})
+Monomer('A20')
+Monomer('IkBa', ['nfkb', 'phos', 'loc'], {'phos': ['u', 'p'], 'loc': ['n', 'c']})
+Monomer('NFkB', ['ikba', 'dna', 'loc'], {'loc': ['n', 'c']})
+Monomer('DNA', ['a20t', 'ikbat'])
+Monomer('A20_mRNA')
+Monomer('IkBa_mRNA')
 
-# solve equations over time tspan
-tspan = np.linspace(0, 10, 101)
-print(tspan)
+Parameter('TNFa_init', 1)
+Parameter('TNFR1_init', 1000)
+Parameter('IKKK_init', 10000)
+Parameter('IKK_init', 200000)
+Parameter('A20_init', 0)
+Parameter('IkBa_init', 0)
+Parameter('NFkB_init', 100000)
+Parameter('DNA_init', 2)
+Parameter('A20_mRNA_init', 1)
+Parameter('IkBa_mRNA_init', 1)
+Initial(TNFa(tnfr1=None), TNFa_init)
+Initial(TNFR1(tnfa=None), TNFR1_init)
+Initial(IKKK(state='n'), IKKK_init)
+Initial(IKK(state='n'), IKK_init)
+Initial(A20, A20_init)
+Initial(IkBa(nfkb=None, phos='u', loc='c'), IkBa_init)
+Initial(NFkB(ikba=None, dna=None, loc='c'), NFkB_init)
+Initial(DNA(a20t=None, ikbat=None), DNA_init)
+Initial(A20_mRNA, A20_mRNA_init)
+Initial(IkBa_mRNA, IkBa_mRNA_init)
+
+
+Parameter('ka20', 10000)
+Parameter('c5', 0.0005)
+Parameter('ka', 0.0001)
+Parameter('ki', 0.01)
+Parameter('k1', 0.000005)
+Parameter('k2', 10000)
+Parameter('k3', 0.003)
+Parameter('k4', 0.0005)
+Parameter('a2', .0000001)
+Parameter('tp', 0.01)
+Parameter('c5a', 0.0001)
+Parameter('a3', 0.0000005)
+Parameter('c6a', 0.00002)
+Parameter('e2a', 0.05)
+Parameter('e1a', 0.005)
+Parameter('i1a', 0.002)
+Parameter('a1', 0.0000005)
+Parameter('c4', 0.5)
+Parameter('c3', 0.00075)
+Parameter('i1', 0.01)
+Parameter('c1', 0.1)
+Parameter('kd', 0.0006)
+Parameter('kb', 0.000004)
+Parameter('q1', 0.00000015)
+Parameter('q2', 0.000001)
+
+Observable('Unbound_TNFa', TNFa(tnfr1=None))
+# Observable('Bound_TNFa', TNFa(tnfr1=1) % TNFR1(tnfa=1))
+# Observable('TNFa_total', TNFa())
+Observable('A20_total', A20())
+
+# Expression('Test', Unbound_TNFa + Bound_TNFa)
+Expression('r_activate', kb * Unbound_TNFa)
+Expression('A20_modifier', ka20 / (ka20 + A20_total))
+Expression('IKKK_activation', ka * A20_modifier)
+Expression('A20_modifier2', (k2 + A20_total) / k2)
+Expression('IKK_inactivation', k3 * A20_modifier2)
+
+Rule('TNFa_binds_TNFR1', TNFa(tnfr1=None) + TNFR1(tnfa=None) | TNFa(tnfr1=1) % TNFR1(tnfa=1), r_activate, kd)
+Rule('TNFR1_activates_IKKK',  TNFa(tnfr1=1) % TNFR1(tnfa=1) + IKKK(state='n') >> IKKK(state='a') +
+     TNFa(tnfr1=1) % TNFR1(tnfa=1), IKKK_activation)
+Rule('IKKKa_deactivates', IKKK(state='a') >> IKKK(state='n'), ki)
+Rule('IKKKa_activates_IKK', IKKK(state='a') + IKK(state='n') >> IKKK(state='a') + IKK(state='a'), k1)
+Rule('IKKa_deactivates', IKK(state='a') >> IKK(state='i'), IKK_inactivation)
+# Rule('IKKa_bind_A20', IKK(state='a') + A20() >> IKK(state='a') % A20(), k2)
+# Rule('IKKa_to_IKKi', IKK(state='a') % A20() >> IKK(state='i') + A20(), k2)
+Rule('IKKi_to_IKKiiIKKn', IKK(state='i') >> IKK(state='n'), k4)
+Rule('IKKa_phos_IkBa', IKK(state='a') + IkBa(nfkb=None, phos='u', loc='c') >> IKK(state='a')
+     + IkBa(nfkb=None, phos='p', loc='c'), a2)
+Rule('IkBa_degraded', IkBa(nfkb=None, phos='p', loc='c') >> None, tp)
+Rule('IkBaNFkB_degraded', IkBa(nfkb=2, phos='u', loc='c') % NFkB(ikba=2, dna=None, loc='c') + IKK(state='a') >>
+     NFkB(ikba=None, dna=None, loc='c') + IkBa(nfkb=None, phos='p', loc='c') + IKK(state='a'), a3)
+Rule('NFkB_Spontaneous', IkBa(nfkb=2, phos='u', loc='c') % NFkB(ikba=2, dna=None, loc='c') >>
+     NFkB(ikba=None, dna=None, loc='c') + IkBa(nfkb=None, phos='u', loc='c'), c6a)
+Rule('NFkB_nuclear_import', NFkB(ikba=None, dna=None, loc='c') >> NFkB(ikba=None, dna=None, loc='n'), i1)
+Rule('IkBaNFkB_formation', NFkB(ikba=None, dna=None, loc='n') + IkBa(nfkb=None, phos='u', loc='n') >>
+     IkBa(nfkb=2, phos='u', loc='n') % NFkB(ikba=2, dna=None, loc='n'), a1)
+Rule('IkBaNFkB_from_ikbat',  NFkB(ikba=None, dna=5, loc='n') % DNA(ikbat=5) + IkBa(nfkb=None, phos='u', loc='n')
+     >> IkBa(nfkb=2, phos='u', loc='n') % NFkB(ikba=2, dna=None, loc='n') + DNA(ikbat=None), q2)
+Rule('IkBaNFkB_from_a20t',  NFkB(ikba=None, dna=5, loc='n') % DNA(a20t=5) + IkBa(nfkb=None, phos='u', loc='n')
+     >> IkBa(nfkb=2, phos='u', loc='n') % NFkB(ikba=2, dna=None, loc='n') + DNA(a20t=None), q2)
+Rule('IkBaNFkB_nuclear_export', IkBa(nfkb=2, phos='u', loc='n') % NFkB(ikba=2, dna=None, loc='n') >>
+     IkBa(nfkb=2, phos='u', loc='c') % NFkB(ikba=2, dna=None, loc='c'), e2a)
+Rule('IkBa_import_export', IkBa(nfkb=None, phos='u', loc='n') | IkBa(nfkb=None, phos='u', loc='c'), e1a, i1a)
+Rule('IkBa_spont_degrad', IkBa(nfkb=None, phos='u', loc='c') >> None, c5a)
+Rule('A20mRNA_spont_degrad', A20_mRNA() >> None, c3)
+Rule('A20_spont_degrad', A20() >> None, c5)
+Rule('IkBamRNA_spont_degrad', IkBa_mRNA() >> None, c3)
+Rule('NFkB_DNA_complex_a20t', NFkB(ikba=None, dna=None, loc='n') + DNA(a20t=None) >>
+     NFkB(ikba=None, dna=5, loc='n') % DNA(a20t=5),q1)
+Rule('NFkB_induces_A20trans', NFkB(ikba=None, dna=5, loc='n') % DNA(a20t=5) >> A20_mRNA(), c1)
+Rule('A20_translation', A20_mRNA() >> A20(), c4)
+Rule('A20_mRNA_degrad', A20_mRNA() >> None, c3)
+Rule('NFkB_DNA_complex_ikbat', NFkB(ikba=None, dna=None, loc='n') + DNA(ikbat=None) >>
+     NFkB(ikba=None, dna=5, loc='n') % DNA(ikbat=5), q1)
+Rule('NFkB_induces_IkBatrans', NFkB(ikba=None, dna=5, loc='n') % DNA(ikbat=5) >> IkBa_mRNA(), c1)
+Rule('IkBa_translation', IkBa_mRNA() >> IkBa(nfkb=None, phos='u', loc='c'), c4)
+Rule('IkBa_mRNA_degrad', IkBa_mRNA() >> None, c3)
+
+
+Observable('Nuclear_NFkB', NFkB(loc='n'))
+Observable('Active_IKK', IKK(state='a'))
+
+# print(model.rules)
+# print(model.parameters)
+# print(model.observables)
+
+tspan = np.linspace(0, 1800, 60)
+# print(tspan)
 x = odesolve(model, tspan, verbose=True)
 
 print(model.species)
-for rxn in model.reactions:
-    print(rxn)
+# for rxn in model.reactions:
+#     print(rxn)
 
-# plot observable results
-plt.figure('My first PySB model')
+plt.figure('NFkB CanB Project')
+
+
+def normalize(values):
+    """
+    Function to generate a normalized version of the observable values in
+    an iterable.
+    """
+    maximum = max(values)
+    return(values / maximum)
+
+print(model.observables)
 for obs in model.observables:
-    plt.plot(tspan, x[obs.name], lw=2, label=obs.name)
+    plt.plot(tspan, normalize(x[obs.name]), lw=2, label=obs.name)
+    print(obs)
 
 plt.legend(loc=0)
 plt.show()
